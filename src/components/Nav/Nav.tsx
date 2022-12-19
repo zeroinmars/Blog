770bc820e0115a107b8f34b0edb27c9836978d25
이승민 <zeroinmars@gmail.com>
2022-12-19 13:48:01 +0900

    navDetail

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 import './Nav.css';
 
-const Nav = () => {
-  
+//Nav 리스트 
+const list= [
+  {img:"web-programming", title:"languages", detail:[{name:"python", img:"python"}, {name:"javascript", img:"javascript"}, {name:"typescript", img:"typescript"},{name:"react", img:"react"},{name:"solidity", img:"solidity"}]},
+  {img:"nft", title:"블록체인", detail:[{name:"뉴스", img:""}, {name:"합의 알고리즘", img:""}, {name:"암호학", img:""},{name:"회사", img:""}]},
+  {img:"icons8-unreal-engine-64", title:"언리얼엔진", detail:[{name:"뉴스/기사", img:""}, {name:"블루프린트", img:""}, {name:"디지털트윈", img:""},{name:"", img:""}]},
+]
 
+const Nav = () => {
   return (
     <div className='nav'>
         <div className="profile">
@@ -12,9 +17,9 @@ const Nav = () => {
           <div style={{textAlign:"center", cursor:"pointer"}}> <img src="./img/app-development.png" width="25px" height="25px" alt="" /> <span style={{fontSize:"20px"}}>ZeroInMars</span></div>
         </div>
         <div className='container'>
-          <NavList img="./img/web-programming.png" content="웹개발" />
-          <NavList img="./img/nft.png" content="블록체인" />
-          <NavList img="./img/icons8-unreal-engine-64.png" content="언리얼엔진" />
+          {list.map(
+              (data,index)=> <NavList key={index} img={data.img} title={data.title} detail={data.detail}/>
+          )}
         </div>
     </div>
   )
@@ -22,29 +27,56 @@ const Nav = () => {
 
 type NavProps = {
   img:string;
-  content:string;
+  title:string;
+  detail: Object;
+}
+type NavDetail = {
+  detail: Object;
 }
 
-interface DetailedHTMLProps {
-  name: string;
+declare module 'react' {
+  interface HTMLAttributes<T>{
+    name?: string;
+  }
 }
 
 
-const NavList = ({img, content}:NavProps) => {
+
+const NavList = ({img, title, detail}:NavProps) => {
+  const navList:any = document.getElementById(title);
+  navList!.style.display = "none";
   const handleListDetail = (e:any) => {
-    console.log(e.target)
+    if(navList.style.display == "block") {
+      navList.style.display = "none";
+    } else {
+      navList.style.display = "block";
+    }
   }
   return(
     <div style={{margin:"5px 0px"}}>
-      <div className="nav-list" data-name={content} onClick={handleListDetail}>
-        <img className='img' src={img} width="25px" height="25px" alt="" />
-        <span>{content}</span>
+      <div className="nav-list" name={title} onClick={handleListDetail}>
+        <img className='img' src={"./img/"+img+".png"} width="25px" height="25px" alt="" />
+        <span>{title}</span>
         <img style={{marginLeft:"20px", marginTop:"5px"}} src="./img/arrow-right.png" width="15px" height="15px" alt="" />
         {/* <img style={{marginLeft:"20px"}} src="./img/arrow-right.png" width="25px" height="25px" alt="" /> */}
       </div>
       <div className='nav-list-hover-bottom'></div>
+      <NavDetail detail={detail} title={title}/>
     </div>
   );
 }
 
+const NavDetail = ({detail, title}:any) => {
+  return(
+    <div id={title}>
+      {detail.map((data:any, index:number)=>(
+        <div key={index} className="navDetail">
+          <img src="./img/arrow-right2.png" alt="" width="7px" height="7px" style={{marginTop:"7px"}}/>
+          <img src={"./img/"+data.name+".png"} width="20px" height="20px" alt="" style={{marginTop:"2px"}}/>
+          <span style={{marginLeft:"10px"}}>{data.name}</span> 
+        </div>
+      ))}
+    </div>
+  );
+}
 export default Nav
