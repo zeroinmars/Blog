82bd3c10b4b1687bda1c5138a83bbbe1439ffe21
이승민 <zeroinmars@gmail.com>
2022-12-24 11:30:27 +0900

    e

--- src/pages/Typescript.tsx
+++ src/pages/Typescript.tsx
@@ -1,8 +1,9 @@
 import Board, {Boards} from '../components/Board';
 import { useSelector } from 'react-redux';
+import { NavListType } from '../modules/reducer';
 
 const Typescript = () => {
-  const typescriptInfo = useSelector(state=>state.navList[0]);
+  const typescriptInfo = useSelector(({navList}:NavListType)=>navList[0]);
   return (
     <div>
       <div style={{height:"500px", width:"100%", background:"url(https://res.cloudinary.com/practicaldev/image/fetch/s---rROvro4--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/y8duykf5peuazf5rghqr.png) fixed"}}>
